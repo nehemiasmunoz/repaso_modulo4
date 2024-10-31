d201bfd2631d0b6b2f485e51251d42fa8b4f879c
mimi <munoz_mar@outlook.com>
2024-10-30 21:02:13 -0300

    Utilizacion de streams

--- src/main/java/com/marvic/Main.java
+++ src/main/java/com/marvic/Main.java
@@ -1,17 +1,31 @@
 package com.marvic;
 
 import com.marvic.modelos.Cliente;
+import com.marvic.utilidad.GsonUtilidad;
 import com.marvic.utilidad.ManejoArchivo;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         //creacion de cliente de prueba
         Cliente cliente = new Cliente("Nehemias", 24);
         //llamado a metodo default toString
-        System.out.println(cliente);
+//        System.out.println(cliente);
         //Escribimos en el archivo clientes.txt de la raiz
         ManejoArchivo.escribir(cliente);
         //Leemos los datos almacenados en el archivo clientes.txt
-        ManejoArchivo.leerArchivo();
+//        ManejoArchivo.leerArchivo();
+
+        //Obtenemos datos desde el archivo
+        List<String> datos = ManejoArchivo.obtenerDatosArchivo();
+        //Utilizamos la funcion stream para iterar sobre la lista aplicando una funcion para cada objeto
+        List<Cliente> clientes = datos.stream()
+                .map(dato -> GsonUtilidad.deserializarObjeto(dato, Cliente.class))
+                .toList();
+        //Imprimos nuestro datos transformados en clientes
+        System.out.println(clientes);
+
+
     }
 }
