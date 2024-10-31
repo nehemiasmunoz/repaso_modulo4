fbfcd7fd9a27cc4355a0beeea941f56fdaa42911
mimi <munoz_mar@outlook.com>
2024-10-30 21:41:03 -0300

    Prueba con un segundo cliente

--- src/main/java/com/marvic/Main.java
+++ src/main/java/com/marvic/Main.java
@@ -10,10 +10,12 @@ public class Main {
     public static void main(String[] args) {
         //creacion de cliente de prueba
         Cliente cliente = new Cliente("Nehemias", 24);
+        Cliente cliente2 = new Cliente("Alex", 21);
         //llamado a metodo default toString
 //        System.out.println(cliente);
         //Escribimos en el archivo clientes.txt de la raiz
         ManejoArchivo.escribir(cliente);
+        ManejoArchivo.escribir(cliente2);
         //Leemos los datos almacenados en el archivo clientes.txt
 //        ManejoArchivo.leerArchivo();
 
